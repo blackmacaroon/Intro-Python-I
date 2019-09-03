db14b6f8da4f1521d16818e32326fda52c975eab
Kayla <6lackmacaroon@gmail.com>
2019-09-03 13:59:32 -0600

    completed 03

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,12 +10,18 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-
+for arg in sys.argv:
+    print(arg)
+'''  '''
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+print(sys.platform)
+''' 'darwin' '''
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+print(sys.version)
+''' 3.7.4 (default, Jul  9 2019, 18:13:23) 
+[Clang 10.0.1 (clang-1001.0.46.4)] '''
 
 
 import os
@@ -23,9 +29,13 @@ import os
 
 # Print the current process ID
 # YOUR CODE HERE
-
+print(os.getpid())
+''' 67457 '''
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+print(os.getcwd())
+''' /Users/kaylacrow/Documents/git/ComputerScience/Python/Intro-Python-I '''
 # Print out your machine's login name
 # YOUR CODE HERE
+print(os.getlogin())
+''' kaylacrow '''
