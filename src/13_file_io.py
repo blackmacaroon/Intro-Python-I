a9a071d228863dce2dd9a32df9fd214867fa5af1
Kayla <6lackmacaroon@gmail.com>
2019-09-04 13:56:06 -0600

    completed 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,20 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
-
+f = open('foo.txt', 'r')
+print(f.read())
+''' 'Do you bite your thumb at us, sir?\nI do bite my thumb, sir.\nDo you bite your thumb at us, sir?\nNo, sir. I do not bite my thumb at you, sir, but I bite my thumb, sir.\nDo you quarrel, sir?\nQuarrel, sir? No, sir.' '''
+f.close()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+f = open('bar.txt', 'w')
+f.write("Is the law on our side, if I say\nay?\nNo.")
+f.close()
+
+f = open('bar.txt', 'r')
+print(f.read())
+f.close()
